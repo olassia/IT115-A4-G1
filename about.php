c0bf153af5b03ca1678c8139376342ee980a0991
timmyd61 <72035662+timmyd61@users.noreply.github.com>
2020-10-30 16:09:54 -0700

    Update about.php

--- about.php
+++ about.php
@@ -10,14 +10,13 @@
             <p>Super cool guy. 
                 blah blah Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.
             </p>
-            <p>Tim</p>
-            <p>Super cool guy. 
-                blah blah Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.
+            <p>Tim Davidson </p>
+            <p>I'm returning to school after a long absence, which scared the bejeezus out of me at first. However, after years of being an actor, a waiter, a manager and a house manager at various times and often simultaneously, I decided it was time to move forward and learn new things. I love the experience of challenging myself again and finding myself rising to the occasion. <br>This bio feels too short, so I should probably mention here that I love cats. I love dogs too, calm down, I just love cats more.<br>And since Thomas brought up things we achieved at 14, I'll tell you that when I was that age I went to the National Spelling Bee. I won't tell you the year, but I will say that when we went to the White House to meet the President we saw a golden retriever there... 
             </p>
             <p>Thomas Mendenhall</p>
             <p>
-<br>Student - Programming
-<br>Inquisitive, curious, driven. 
+<br>Student - Programmin
+<br>Inquisitive, curious, driven.
 <br>See pandemic for reason of unemployment. 
 <br>Seeing to wildlife and the environment protected while advancing quality of life for human beings is my purpose.  
 <br>I have lead teams up to 15 people and coordinated with multiple departments simultaneously.
